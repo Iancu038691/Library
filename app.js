dc0b07b0af68f93718f3ee8061ef6a9bc451d958
Iancu Stefan-George <iancu.stefan038691@gmail.com>
2023-03-20 22:04:19 +0200

    add content to html

--- app.js
+++ app.js
@@ -15,3 +15,28 @@ addBookToLibrary('Games of Throne', 'George R.R. Martin', '373', 'not yet');
 for (const key in myLibrary) {
   console.log(myLibrary[key].author);
 }
+
+const container = document.querySelector('.container');
+container.appendChild(document.createElement('tbody'));
+const tr = document.querySelector('tr');
+tr.classList.add('qwerty');
+const bookTitle = document.createElement('td');
+bookTitle.classList.add('title');
+bookTitle.textContent = myLibrary[0].title;
+
+const bookAuthor = document.createElement('td');
+bookAuthor.classList.add('author');
+bookAuthor.textContent = myLibrary[0].author;
+
+const bookPages = document.createElement('td');
+bookPages.classList.add('pages');
+bookPages.textContent = myLibrary[0].pages;
+
+const bookStatus = document.createElement('td');
+bookStatus.classList.add('status');
+bookStatus.textContent = myLibrary[0].status;
+
+tr.appendChild(bookTitle)
+  .appendChild(bookAuthor)
+  .appendChild(bookPages)
+  .appendChild(bookStatus);
